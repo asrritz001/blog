f3ad467db97cb135c8fd5c81393390c3403b668a
Ritika khatri <asr.ritika001@gmail.com>
2024-07-01 00:31:20 -0400

    createblog

---
+++ src/components/Createblog.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react"; 
+import { Navigate, useNavigate } from "react-router-dom";
+
+const CreateBlog =() =>{
+    const[title, setTitle] =useState("");
+    const[content, setContent] =useState("");
+    const navigate =useNavigate();
+
+    const handleSubmit =  async (e) => {
+        e.preventDefault();
+        try{
+            await addBlog (Collection(db,"posts"),{
+                title,
+                content,
+                createAt:new Date(),
+            });
+            Navigate("/");
+        }catch(error ){
+            console.log("Error adding document",error);
+        }
+
+    };
+    return(
+        <div className="container">
+            <h1>Craete a new blog </h1>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="Title"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                    required />
+
+                    <textarea 
+                    placeholder="Content"
+                    value={content}
+                    onChange={(e) => setContent(e.taget.value)}
+                    required />
+                    <button type="submit">Submit</button>
+                        </form>
+        </div>
+    );
+
+};
